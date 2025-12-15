bf058d4ff85586f26b59c886fa033c6cecb708c7
Inaki Villar <inaki.seri@gmail.com>
2025-12-15 19:46:07 -0400

    r8 process

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -42,6 +42,23 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("com.gradle.develocity") version "4.3"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com/"
+    allowUntrustedServer = true
+    edgeDiscovery = true
+    buildScan {
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { "0.0.0.0" } }
+        }
+    }
+}
 rootProject.name = "nowinandroid"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
