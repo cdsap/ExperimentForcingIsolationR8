25dccd044246301ed2e6b0827d74acfc4f9668fb
Inaki Villar <inaki.seri@gmail.com>
2025-12-17 18:23:00 -0400

    using correct version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
     alias(libs.plugins.baselineprofile)
     alias(libs.plugins.roborazzi)
     alias(libs.plugins.kotlin.serialization)
-    id("io.github.cdsap.r8booster") version "0.0.2"
+    id("io.github.cdsap.r8booster") version "0.0.3"
 }
 
 android {
